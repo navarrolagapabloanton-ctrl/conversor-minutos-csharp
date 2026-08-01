5c39a97467f6c768ee810965c580c70ed95a1b2e
Pablo <navarrolagapabloanton@gmail.com>
2026-08-01 13:44:55 +0200

    Agregar archivos de proyecto.

---
+++ ConversorMinutosApp/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 1. Pida al usuario una cantidad de minutos: Ingresar minutos.
+ * 2. Calcule cuántos segundos son: Multiplicar los minutos ingresados por 60.
+ * 3. Muestre el resultado: Salida en consola.
+ */
+
+Console.WriteLine("CONVERSOR DE MINUTOS EN SEGUNDOS\n");
+Console.WriteLine("Ingrese los minutos que quieres transformar en segundos:");
+
+int minutos;
+int segundos;
+
+string? entrada = Console.ReadLine();
+
+while (!int.TryParse(entrada, out minutos) || minutos < 0)
+{
+    Console.WriteLine("Valor no válido. Introduce un número igual o mayor" +
+        " que 0:");
+
+    entrada = Console.ReadLine();
+}
+
+segundos = minutos * 60;
+
+Console.WriteLine($"Son {segundos} segundos.");
