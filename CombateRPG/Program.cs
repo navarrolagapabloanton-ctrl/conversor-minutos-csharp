697d1b11aa9b2331feac98470323a37534e7e83d
Pablo <navarrolagapabloanton@gmail.com>
2026-08-24 20:12:03 +0200

    Añado dos programas: Cuenta Bancaria y Combate RPG

---
+++ CombateRPG/Program.cs
@@ -0,0 +1,149 @@
+﻿/*COMBATE RPG
+ * 
+ * 1. Atacar enemigo: Restar int.
+ * 2. Curar héroe: Sumar int.
+ * 3. Mostrar estado: Muestra si está vivo o muerto.
+ * 4. Salir.
+ * */
+
+Personaje heroe = new Personaje("Pablo Crack", 100);
+Personaje goblin = new Personaje("Goblin", 50);
+
+while(true)
+{ 
+    MenuCombate();
+
+    Console.WriteLine("Seleccione una opción del 1 al 4.");
+
+    string? entrada = Console.ReadLine();
+
+    switch(entrada)
+    {
+        case "1":
+            if(!goblin.EstaVivo())
+            {
+                Console.WriteLine("¡El goblin ya está muerto!");
+                return;
+            }
+
+            Console.WriteLine("¿Cuántos puntos de ataque" +
+                " tienes?");
+
+            entrada = Console.ReadLine();
+
+            int puntosAtaque;
+
+            while(!int.TryParse(entrada, out puntosAtaque) ||
+                puntosAtaque < 0)
+            {
+                Console.WriteLine("Puntos de ataque no" +
+                    " válidos. Introduce otra cantidad.");
+                entrada = Console.ReadLine();
+            }
+
+            goblin.RecibirDanio(puntosAtaque);
+
+            if(!goblin.EstaVivo())
+            {
+                Console.WriteLine("¡Has derrotado al Goblin!");
+            }
+
+            break;
+
+        case "2":
+            Console.WriteLine("¿Cuántos puntos de vida" +
+                " quieres recuperarte?");
+
+            entrada = Console.ReadLine();
+
+            int puntosVida;
+
+            while(!int.TryParse(entrada, out puntosVida) ||
+                puntosVida < 0)
+            {
+                Console.WriteLine("Puntos de vida no válidos." +
+                    " Introduce otra cantidad.");
+                entrada = Console.ReadLine();
+            }
+
+            heroe.Curar(puntosVida);
+            break;
+
+        case "3":
+            heroe.MostrarEstado();
+            goblin.MostrarEstado();
+            break;
+
+        case "4":
+            Console.WriteLine("Huyes del combate...");
+            return;
+
+        default:
+            Console.WriteLine("Selecciona una de las" +
+                " acciones del 1 al 4.");
+            break;
+    }
+}
+
+static void MenuCombate()
+{
+    Console.WriteLine("\n¡ENTRAS EN COMBATE!\n");
+
+    Console.WriteLine("1. Atacar enemigo.");
+    Console.WriteLine("2. Curar héroe.");
+    Console.WriteLine("3. Mostrar estado.");
+    Console.WriteLine("4. Escapar.\n");
+}
+
+class Personaje
+{
+    public string Nombre { get; set; }
+    public int Vida { get; set; }
+
+    public Personaje(string nombre, int vida)
+    {
+        Nombre = nombre;
+        Vida = vida;
+    }
+
+    public void RecibirDanio(int danio)
+    {
+        Vida -= danio;
+
+        if (Vida < 0)
+        {
+            Vida = 0;
+        }
+
+        Console.WriteLine($"{Nombre} recibe {danio} de daño.");
+        Console.WriteLine($"Le quedan {Vida} puntos de vida.");
+    }
+
+    public void Curar(int cantidad)
+    {
+        Vida += cantidad;
+        Console.WriteLine($"{Nombre} recibe {cantidad} puntos" +
+            $" de salud.");
+        Console.WriteLine($"Ahora tiene {Vida} puntos de vida.");
+    }
+
+    public void MostrarEstado()
+    {
+        Console.WriteLine($"\nNombre: {Nombre}.");
+        Console.WriteLine($"PS: {Vida}.");
+
+        if(EstaVivo())
+        {
+            Console.WriteLine("Vivo.");
+        }
+        else
+        {
+            Console.WriteLine("Muerto.");
+        }
+    }
+
+    public bool EstaVivo()
+    {
+        return Vida > 0;
+    }
+}
---
+++ CuentaBancaria/Program.cs
@@ -0,0 +1,133 @@
+﻿/*CUENTA BANCARIA
+ * 1. Consultar cuenta.
+ * 2. Ingresar dinero: No acepta cantidades <= 0.
+ * 3. Retirar dinero: No acepta cantidades <= 0 ni superiores al saldo.
+ * 4. Salir.
+ */
+
+CuentaBancaria cuentaPablo = new CuentaBancaria("Pablo Navarro",
+    1000.75m);
+
+while(true)
+{
+    MenuCuentaBancaria();
+
+    Console.WriteLine("Seleccione una de las opciones del 1 al 4.");
+
+    string? entrada = Console.ReadLine();
+
+    switch(entrada)
+    {
+        case "1":
+            cuentaPablo.MostrarInformacion();
+            break;
+
+        case "2":
+            {
+                Console.WriteLine("\nIngrese la cantidad de dinero" +
+                    " a ingresar.");
+
+                decimal cantidadIngresada;
+
+                entrada = Console.ReadLine();
+
+                while(!decimal.TryParse(entrada, 
+                    out cantidadIngresada))
+                {
+                    Console.WriteLine("\nIngrese una cantidad" +
+                        " numérica.");
+                    entrada = Console.ReadLine();
+                }
+
+                cuentaPablo.Ingresar(cantidadIngresada);
+                break;
+            }
+
+        case "3":
+            {
+                Console.WriteLine("\nIngrese la cantidad de dinero" +
+                    " a retirar.");
+
+                decimal cantidadRetirada;
+
+                entrada = Console.ReadLine();
+
+                while (!decimal.TryParse(entrada,
+                    out cantidadRetirada))
+                {
+                    Console.WriteLine("\nIngrese una cantidad" +
+                        " numérica.");
+                    entrada = Console.ReadLine();
+                }
+
+                cuentaPablo.Retirar(cantidadRetirada);
+                break;
+            }
+
+        case "4":
+            Console.WriteLine("\nSaliendo...");
+            return;
+
+        default:
+            Console.WriteLine("\nElige una de las opciones del 1 al" +
+                " 4.");
+            break;
+    }
+}
+
+static void MenuCuentaBancaria()
+{
+    Console.WriteLine("\nCUENTA BANCARIA\n");
+
+    Console.WriteLine("1. Consultar cuenta.");
+    Console.WriteLine("2. Ingresar dinero.");
+    Console.WriteLine("3. Retirar dinero.");
+    Console.WriteLine("4. Salir.\n");
+
+}
+class CuentaBancaria
+{
+    public string Titular { get; set; }
+    public decimal Saldo { get; set; }
+
+    public CuentaBancaria(string titular, decimal saldoInicial)
+    {
+        Titular = titular;
+        Saldo = saldoInicial;
+    }
+
+    public void Ingresar(decimal cantidad)
+    {
+        if (cantidad > 0)
+        {
+            Saldo += cantidad;
+            Console.WriteLine($"\nNuevo saldo: {Saldo:F2}€.");
+        }
+        else
+        {
+            Console.WriteLine("\nCantidad ingresada incorrecta." +
+                " Añade una cantidad mayor a 0.");
+        }
+    }
+
+    public void Retirar(decimal cantidad)
+    {
+        if (cantidad > 0 && cantidad <= Saldo)
+        {
+            Saldo -= cantidad;
+            Console.WriteLine($"\nNuevo saldo: {Saldo:F2}€.");
+        }
+        else
+        {
+            Console.WriteLine("\nCantidad ingresada incorrecta." +
+                " Añade una cantidad mayor a 0 y menor" +
+                " o igual a tu saldo.");
+        }
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"\nTitular: {Titular}.");
+        Console.WriteLine($"Saldo: {Saldo:F2}€.");
+    }
+}
